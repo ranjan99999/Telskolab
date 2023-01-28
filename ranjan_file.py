2d3daeefb58f61fe0260a8bc976d9d38a6e797de
ranjan99999 <ranjansingh1114@gmail.com>
2023-01-28 21:39:04 +0530

    I have added a new program of sorting of list without using sort functions

--- ranjan_file.py
+++ ranjan_file.py
@@ -1,2 +1,9 @@
 string = "hello how are you"
 print(string)
+
+my_list = [2, 1, 3, 5, 3, 9, 8, 5, 73, 2, 6]
+for i in range(0, len(my_list)):
+    for j in range(i + 1, len(my_list)):
+        if my_list[i] >= my_list[j]:
+         my_list[i], my_list[j] = my_list[j], my_list[i]
+print(my_list)
